8e70c20048b4123d6fa11b516601ca9ddd7fe9bc
Santosh Yadav <santosh.yadav198613@gmail.com>
2023-08-07 22:30:21 +0200

    add netlify config

---
+++ vite.config.ts
@@ -0,0 +1,35 @@
+/// <reference types="vitest" />
+
+import { defineConfig } from 'vite';
+import analog from '@analogjs/platform';
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => ({
+  publicDir: 'src/assets',
+  build: {
+    target: ['es2020'],
+  },
+  resolve: {
+    mainFields: ['module'],
+  },
+  plugins: [
+    analog({
+      nitro: {
+        preset: 'netlify',
+        output: {
+          serverDir: '/.netlify/functions-internal',
+          publicDir: 'dist',
+        },
+      },
+    }),
+  ],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['src/test.ts'],
+    include: ['**/*.spec.ts'],
+  },
+  define: {
+    'import.meta.vitest': mode !== 'production',
+  },
+}));
